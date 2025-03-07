d75189f86f8ffdbe86203e4a4bc3cf5edb8ed784
Oleksandr Koverza <alexandrkoverza@gmail.com>
2025-03-07 13:50:09 +0200

    test 3[2025-03-07 13:50:10][main]

--- src/js/app.js
+++ src/js/app.js
@@ -35,6 +35,16 @@ import { swipe, formTelegram, i18n, sendServer } from './$/index.js';
 window.addEventListener('load', () => {
     // formTelegram()
 
+
+
+
+
+
+
+
+
+    
+
     document.getElementById('telegramForm').addEventListener('submit', async function (e) {
         e.preventDefault(); // Предотвращаем перезагрузку страницы
 
