194ac5caf3862fc2540f0ae64f4ece52aaf9f16a
Oleksandr Koverza <alexandrkoverza@gmail.com>
2025-03-22 20:51:31 +0200

    22.03.25 20:51 -

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,11 +1,7 @@
-// Основной модуль
 import gulp from 'gulp';
-// Импорт путей
 import { path } from './gulp/config/path.js';
-// Импорт общих плагинов
 import { plugins } from './gulp/config/plugins.js';
 
-// Передаем значения в глобальную переменную
 global.app = {
     isBuild: process.argv.includes('--build'),
     isDev: !process.argv.includes('--build'),
@@ -14,7 +10,6 @@ global.app = {
     plugins: plugins
 };
 
-// Импорт задач
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
@@ -27,33 +22,27 @@ import { svgSpriteTask } from './gulp/tasks/svg-sprite.js';
 import { zip } from './gulp/tasks/zip.js';
 import { ftp } from './gulp/tasks/ftp.js';
 
-// Наблюдатель за изменениями в файлах
 function watcher() {
     gulp.watch(path.watch.files, copy);
-    gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
+    gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch.images, images);
 }
 
-// Последовательная обработака шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
-// Основные задачи
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, svgSpriteTask));
 
-// Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
 const deployFTP = gulp.series(reset, mainTasks, ftp);
 
-// Экспорт сценариев
 export { svgSpriteTask };
 export { dev };
 export { build };
 export { deployZIP };
 export { deployFTP };
 
-// Выполнение сценария по умолчанию
 gulp.task('default', dev);
