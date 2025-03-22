ffb5b245400b7aaea2a70462cf71a2d276b860c4
Oleksandr Koverza <alexandrkoverza@gmail.com>
2025-03-22 18:31:04 +0200

    22.03.25 18:31 -

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,46 @@
 // Основной модуль
-import gulp from "gulp";
+import gulp from 'gulp';
 // Импорт путей
-import { path } from "./gulp/config/path.js";
+import { path } from './gulp/config/path.js';
 // Импорт общих плагинов
-import { plugins } from "./gulp/config/plugins.js";
+import { plugins } from './gulp/config/plugins.js';
 
 // Передаем значения в глобальную переменную
 global.app = {
-  isBuild: process.argv.includes("--build"),
-  isDev: !process.argv.includes("--build"),
-  path: path,
-  gulp: gulp,
-  plugins: plugins,
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins
 };
 
 // Импорт задач
-import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js";
-import { html } from "./gulp/tasks/html.js";
-import { server } from "./gulp/tasks/server.js";
-import { scss } from "./gulp/tasks/scss.js";
-import { js } from "./gulp/tasks/js.js";
-import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
-import { svgSpriteTask } from "./gulp/tasks/svg-sprite.js";
-import { zip } from "./gulp/tasks/zip.js";
-import { ftp } from "./gulp/tasks/ftp.js";
+const { copy } = await import('./gulp/tasks/copy.js');
+const { reset } = await import('./gulp/tasks/reset.js');
+const { html } = await import('./gulp/tasks/html.js');
+const { server } = await import('./gulp/tasks/server.js');
+const { scss } = await import('./gulp/tasks/scss.js');
+const { js } = await import('./gulp/tasks/js.js');
+const { images } = await import('./gulp/tasks/images.js');
+const { otfToTtf, ttfToWoff, fontsStyle } = await import('./gulp/tasks/fonts.js');
+const { svgSpriteTask } = await import('./gulp/tasks/svg-sprite.js');
+const { zip } = await import('./gulp/tasks/zip.js');
+const { ftp } = await import('./gulp/tasks/ftp.js');
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
-  gulp.watch(path.watch.files, copy);
-  gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
-  gulp.watch(path.watch.scss, scss);
-  gulp.watch(path.watch.js, js);
-  gulp.watch(path.watch.images, images);
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
 }
 
 // Последовательная обработака шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(
-  fonts,
-  gulp.parallel(copy, html, scss, js, images, svgSpriteTask)
-);
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, svgSpriteTask));
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
@@ -51,12 +48,8 @@ const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
 const deployFTP = gulp.series(reset, mainTasks, ftp);
 
-// Экспорт сценариев
-export { svgSpriteTask };
-export { dev };
-export { build };
-export { deployZIP };
-export { deployFTP };
-
 // Выполнение сценария по умолчанию
-gulp.task("default", dev);
+gulp.task('default', dev);
+
+// Экспорт сценариев
+export { svgSpriteTask, dev, build, deployZIP, deployFTP };
